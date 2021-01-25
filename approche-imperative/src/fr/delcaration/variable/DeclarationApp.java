8233f3f7f49e95dd942e3918110172692efe80cb
sylvain <lardiere.sylvain5@gmail.com>
2021-01-25 14:46:36 +0100

    ajout d'une partie des exos

--- approche-imperative/src/fr/delcaration/variable/DeclarationApp.java
+++ approche-imperative/src/fr/delcaration/variable/DeclarationApp.java
@@ -3,20 +3,10 @@ package fr.delcaration.variable;
 public class DeclarationApp {
 
 	public static void main(String[] args) {
-		byte v1 = 12;
-		short v2 = 23322;
-		int v3 = 2343829;
-		long v4 = 123232323L;
-		float v5 = 1.2F;
-		double v6 = 2.50D;
-		char v7 = 'k';
-		boolean v8 = true;
-		String v9 = "bonjour Mr.";
-		
-		System.out.println("double => " + v6);
-		System.out.println("Formation cool : " + v8);
-		
-		String randomString ="Voici le résultat d'un calcul: \n1+5 = 6";
-		System.out.println(randomString);
+		int nombre = 5;
+		int nombre2;
+		nombre2=nombre++;
+		System.out.println(nombre2); //5
+		System.out.println(nombre); //6
 	}
 }
