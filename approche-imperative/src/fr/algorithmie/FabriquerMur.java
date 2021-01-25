81b9f4a5d912bd9157c328635915f9519ea88611
sylvain <lardiere.sylvain5@gmail.com>
2021-01-25 16:32:07 +0100

    v5

--- approche-imperative/src/fr/algorithmie/FabriquerMur.java
+++ approche-imperative/src/fr/algorithmie/FabriquerMur.java
@@ -1,5 +1,17 @@
 package fr.algorithmie;
 
 public class FabriquerMur {
-
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("ok");
+	}
+	
+	
+    static boolean fabriquerMur (int nbSmall, int nbBig, int longueur ) {
+    	int divisonSmall = longueur / nbSmall;
+    	int divisonBig = longueur / nbBig;
+    	
+    	return true;
+    }
 }
